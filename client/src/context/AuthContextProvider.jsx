c7c8c5a75be0eaaf7d54fb2dad6c40d919ecaea7
Pushkar2912 <pushkar2912@gmail.com>
2024-11-12 13:15:41 +0530

    feat: auth

--- client/src/context/AuthContextProvider.jsx
+++ client/src/context/AuthContextProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react'
-import { logIn, signUp } from '../api/auth'
+import React, { createContext, useEffect, useState } from 'react'
+import { getMe, logIn, signUp } from '../api/auth'
 import { useNavigate } from 'react-router-dom'
 import { PATHS } from '../utils/paths';
 
@@ -27,14 +27,37 @@ const AuthContextProvider = ({ children }) => {
       const { data } = await logIn(values);
       setUser(data.userWithoutPassword);
       localStorage.setItem('token', data.token)
+      window.location.reload();
     } catch (error) {
       console.log(error.message)
     }
   }
 
+  const handleLogout = async() => {
+    try {
+      localStorage.removeItem('token');
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+   
+  useEffect(() => {
+    getMe().then(({data}) => {
+      setUser(data.user);
+      console.log("data from useEffect", data)
+      navigate(PATHS.HOME);
+    }).catch((error) =>{
+      console.log(error.message)
+      navigate(PATHS.LOGIN)
+    })
+  },[])
+
   const values = {
+    user,
     handleSignUp,
-    handleLogin
+    handleLogin,
+    handleLogout
   }
   return (
     <AuthContext.Provider value={values}>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContextProvider'
 
 const Home = () => {
+
+  const { handleLogout } = useContext(AuthContext);
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <button onClick={handleLogout} className='p-2 bg-blue-400 text-slate-100 rounded-md font-medium'>Logout</button>
+    </div>
   )
 }
 
